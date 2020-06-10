f5f1b8a970074d4ce1d948531d71e725de682c81
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2020-06-11 00:47:59 +0200

    PoC - creating new structure

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="carousel">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" src="./_carousel.scss" />
---
+++ src/components/Slide/Slide.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slide">
+    slide
+  </div>
+</template>
+
+<style lang="scss" src="./_slide.scss" />
