f0a0b6d9135aad8ca5064958ac77cd219fbf405d
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2020-06-12 14:00:37 +0200

    adding vuepress with example content

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'VueSnap',
+  description: 'Lightweight Slider/Carousel based on CSS Scroll Snap and Vue',
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+      // { text: 'Examples', link: '/examples/' },
+      // { text: 'API', link: '/api/' }s
+    ]
+  }
+}
