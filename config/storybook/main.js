a0535898b15caee017a30338c75d73bf8d90ef5e
Bartosz Dominiak <smugers@gmail.com>
2020-06-12 18:58:45 +0200

    Feature/storybook docs (#10)
    * adding storybook addons and initialize
    
    * accesibility fixes
    
    * added carousel example with images, added props preview in docs, updated examples styles, added props/slots descriptions
    
    * added knobs based on props

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,5 +1,13 @@
 module.exports = {
   stories: ['../../src/**/*.stories.@(js|mdx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs'
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
