3d7b7acd1519dd2332d7ee8eead14e118d128bab
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2020-06-16 02:04:04 +0200

    deleted vue-style-loader and sass-loader from findal bundle

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -6,6 +6,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
   output: {
     filename: 'vue-snap.min.js'
   },
