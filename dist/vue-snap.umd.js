7bc4bfd555f43219c1e6fc2eb5b1aae413b664c5
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2020-06-23 23:37:59 +0200

    generated dist files (prevent pipelines errors)

---
+++ dist/vue-snap.umd.js
@@ -0,0 +1,390 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('seamless-scroll-polyfill/dist/esm/Element.scrollBy')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'seamless-scroll-polyfill/dist/esm/Element.scrollBy'], factory) :
+  (global = global || self, factory(global.VueSnap = {}, global.Element_scrollBy));
+}(this, (function (exports, Element_scrollBy) { 'use strict';
+
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  var script = {
+    props: {
+      /**
+       * Direction (by default Horizontal)
+       */
+      vertical: {
+        type: Boolean,
+        default: false
+      },
+
+      /**
+       * Navigation Arrows
+       */
+      navigation: {
+        type: Boolean,
+        default: true
+      },
+
+      /**
+       * Scroll per page, not per item
+       */
+      scrollPage: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      getClass: function getClass() {
+        return ['vs-carousel', {
+          'vs-carousel--vertical': this.vertical
+        }];
+      }
+    },
+    mounted: function mounted() {
+      this.scroll(); // Dirty hack to force rerender CSS Scroll Snap position
+    },
+    methods: {
+      changeSlide: function changeSlide(direction) {
+        var _ref = this.scrollPage ? this.$refs.vsWrapper : this.$children[0].$el,
+            offsetWidth = _ref.offsetWidth,
+            offsetHeight = _ref.offsetHeight;
+
+        if (this.vertical) {
+          this.scroll(0, direction * offsetHeight);
+          return;
+        }
+
+        this.scroll(direction * offsetWidth, 0);
+      },
+      scroll: function scroll() {
+        var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+        var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+        this.$refs.vsWrapper.scrollBy({
+          left: x,
+          top: y,
+          behavior: 'smooth'
+        });
+      }
+    }
+  };
+
+  function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+      if (typeof shadowMode !== 'boolean') {
+          createInjectorSSR = createInjector;
+          createInjector = shadowMode;
+          shadowMode = false;
+      }
+      // Vue.extend constructor export interop.
+      const options = typeof script === 'function' ? script.options : script;
+      // render functions
+      if (template && template.render) {
+          options.render = template.render;
+          options.staticRenderFns = template.staticRenderFns;
+          options._compiled = true;
+          // functional template
+          if (isFunctionalTemplate) {
+              options.functional = true;
+          }
+      }
+      // scopedId
+      if (scopeId) {
+          options._scopeId = scopeId;
+      }
+      let hook;
+      if (moduleIdentifier) {
+          // server build
+          hook = function (context) {
+              // 2.3 injection
+              context =
+                  context || // cached call
+                      (this.$vnode && this.$vnode.ssrContext) || // stateful
+                      (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+              // 2.2 with runInNewContext: true
+              if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                  context = __VUE_SSR_CONTEXT__;
+              }
+              // inject component styles
+              if (style) {
+                  style.call(this, createInjectorSSR(context));
+              }
+              // register component module identifier for async chunk inference
+              if (context && context._registeredComponents) {
+                  context._registeredComponents.add(moduleIdentifier);
+              }
+          };
+          // used by ssr in case component is cached and beforeCreate
+          // never gets called
+          options._ssrRegister = hook;
+      }
+      else if (style) {
+          hook = shadowMode
+              ? function (context) {
+                  style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+              }
+              : function (context) {
+                  style.call(this, createInjector(context));
+              };
+      }
+      if (hook) {
+          if (options.functional) {
+              // register for functional component in vue file
+              const originalRender = options.render;
+              options.render = function renderWithStyleInjection(h, context) {
+                  hook.call(context);
+                  return originalRender(h, context);
+              };
+          }
+          else {
+              // inject component registration as beforeCreate hook
+              const existing = options.beforeCreate;
+              options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+          }
+      }
+      return script;
+  }
+
+  const isOldIE = typeof navigator !== 'undefined' &&
+      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+  function createInjector(context) {
+      return (id, style) => addStyle(id, style);
+  }
+  let HEAD;
+  const styles = {};
+  function addStyle(id, css) {
+      const group = isOldIE ? css.media || 'default' : id;
+      const style = styles[group] || (styles[group] = { ids: new Set(), styles: [] });
+      if (!style.ids.has(id)) {
+          style.ids.add(id);
+          let code = css.source;
+          if (css.map) {
+              // https://developer.chrome.com/devtools/docs/javascript-debugging
+              // this makes source maps inside style tags work properly in Chrome
+              code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
+              // http://stackoverflow.com/a/26603875
+              code +=
+                  '\n/*# sourceMappingURL=data:application/json;base64,' +
+                      btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+                      ' */';
+          }
+          if (!style.element) {
+              style.element = document.createElement('style');
+              style.element.type = 'text/css';
+              if (css.media)
+                  style.element.setAttribute('media', css.media);
+              if (HEAD === undefined) {
+                  HEAD = document.head || document.getElementsByTagName('head')[0];
+              }
+              HEAD.appendChild(style.element);
+          }
+          if ('styleSheet' in style.element) {
+              style.styles.push(code);
+              style.element.styleSheet.cssText = style.styles
+                  .filter(Boolean)
+                  .join('\n');
+          }
+          else {
+              const index = style.ids.size - 1;
+              const textNode = document.createTextNode(code);
+              const nodes = style.element.childNodes;
+              if (nodes[index])
+                  style.element.removeChild(nodes[index]);
+              if (nodes.length)
+                  style.element.insertBefore(textNode, nodes[index]);
+              else
+                  style.element.appendChild(textNode);
+          }
+      }
+  }
+
+  /* script */
+  const __vue_script__ = script;
+
+  /* template */
+  var __vue_render__ = function() {
+    var _vm = this;
+    var _h = _vm.$createElement;
+    var _c = _vm._self._c || _h;
+    return _c(
+      "div",
+      { class: _vm.getClass },
+      [
+        _c(
+          "div",
+          { ref: "vsWrapper", staticClass: "vs-carousel__wrapper" },
+          [_vm._t("default")],
+          2
+        ),
+        _vm._v(" "),
+        _vm.navigation
+          ? [
+              _vm._t("navigation", [
+                _c("button", {
+                  ref: "vsNavigationLeft",
+                  staticClass:
+                    "\n          vs-carousel__navigation\n          vs-carousel__navigation--left\n        ",
+                  attrs: { "aria-label": "Slide left" },
+                  on: {
+                    click: function($event) {
+                      return _vm.changeSlide(-1)
+                    }
+                  }
+                }),
+                _vm._v(" "),
+                _c("button", {
+                  ref: "vsNavigationRight",
+                  staticClass:
+                    "\n          vs-carousel__navigation\n          vs-carousel__navigation--right\n        ",
+                  attrs: { "aria-label": "Slide right" },
+                  on: {
+                    click: function($event) {
+                      return _vm.changeSlide(1)
+                    }
+                  }
+                })
+              ])
+            ]
+          : _vm._e()
+      ],
+      2
+    )
+  };
+  var __vue_staticRenderFns__ = [];
+  __vue_render__._withStripped = true;
+
+    /* style */
+    const __vue_inject_styles__ = function (inject) {
+      if (!inject) return
+      inject("data-v-80e179a2_0", { source: ".vs-carousel {\n    position: relative;\n}\n.vs-carousel--vertical .vs-carousel__wrapper {\n    overflow-x: hidden;\n    overflow-y: scroll;\n    scroll-snap-type: y mandatory;\n    flex-direction: column;\n}\n.vs-carousel__wrapper {\n    display: flex;\n    height: 200px;\n    overflow-x: scroll;\n    overflow-y: hidden;\n    scroll-snap-type: x mandatory;\n    scroll-behavior: smooth;\n    -webkit-overflow-scrolling: touch;\n    -ms-overflow-style: -ms-autohiding-scrollbar;\n    scrollbar-width: none;\n    -ms-overflow-style: none;\n}\n.vs-carousel__wrapper::-webkit-scrollbar {\n    display: none;\n}\n.vs-carousel__slide {\n    flex: 0 0 100%;\n    height: 100%;\n    scroll-snap-align: start;\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    outline: none;\n}\n.vs-carousel__navigation {\n    position: absolute;\n    top: 50%;\n    transform: translateY(-50%);\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    min-width: 48px;\n    min-height: 48px;\n    background-image: url(\"data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M14 3L6 10L14 17' stroke='%232F2F2F' stroke-width='1'/%3E%3C/svg%3E%0A\");\n    background-repeat: no-repeat;\n    background-size: 20px 20px;\n    background-position: center;\n    background-color: rgba(255, 255, 255, 0.8);\n    border: 0;\n    padding: 0;\n    outline: none;\n    cursor: pointer;\n}\n.vs-carousel__navigation:hover,\n.vs-carousel__navigation:focus {\n    background-color: white;\n}\n.vs-carousel__navigation--left {\n    left: 0;\n}\n.vs-carousel__navigation--right {\n    right: 0;\n    transform: rotate(180deg) translateY(50%);\n}\n", map: undefined, media: undefined });
+
+    };
+    /* scoped */
+    const __vue_scope_id__ = undefined;
+    /* module identifier */
+    const __vue_module_identifier__ = undefined;
+    /* functional template */
+    const __vue_is_functional_template__ = false;
+    /* style inject SSR */
+    
+    /* style inject shadow dom */
+    
+
+    
+    const __vue_component__ = /*#__PURE__*/normalizeComponent(
+      { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+      __vue_inject_styles__,
+      __vue_script__,
+      __vue_scope_id__,
+      __vue_is_functional_template__,
+      __vue_module_identifier__,
+      false,
+      createInjector,
+      undefined,
+      undefined
+    );
+
+  /* script */
+
+  /* template */
+  var __vue_render__$1 = function() {
+    var _vm = this;
+    var _h = _vm.$createElement;
+    var _c = _vm._self._c || _h;
+    return _c(
+      "div",
+      {
+        ref: "vsSlide",
+        staticClass: "vs-carousel__slide",
+        attrs: { tabindex: "0" }
+      },
+      [_vm._t("default")],
+      2
+    )
+  };
+  var __vue_staticRenderFns__$1 = [];
+  __vue_render__$1._withStripped = true;
+
+    /* style */
+    const __vue_inject_styles__$1 = undefined;
+    /* scoped */
+    const __vue_scope_id__$1 = undefined;
+    /* module identifier */
+    const __vue_module_identifier__$1 = undefined;
+    /* functional template */
+    const __vue_is_functional_template__$1 = false;
+    /* style inject */
+    
+    /* style inject SSR */
+    
+    /* style inject shadow dom */
+    
+
+    
+    const __vue_component__$1 = /*#__PURE__*/normalizeComponent(
+      { render: __vue_render__$1, staticRenderFns: __vue_staticRenderFns__$1 },
+      __vue_inject_styles__$1,
+      {},
+      __vue_scope_id__$1,
+      __vue_is_functional_template__$1,
+      __vue_module_identifier__$1,
+      false,
+      undefined,
+      undefined,
+      undefined
+    );
+
+  // Polyfill: SmoothScroll
+
+  if (!('scrollBehavior' in document.documentElement.style)) {
+    Element_scrollBy.polyfill();
+  }
+
+  var install = function install(Vue) {
+    Vue.component('carousel', __vue_component__);
+    Vue.component('slide', __vue_component__$1);
+  };
+
+  var index = {
+    install: install
+  };
+
+  exports.Carousel = __vue_component__;
+  exports.Slide = __vue_component__$1;
+  exports.default = index;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
