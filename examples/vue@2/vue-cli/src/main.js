fa7804ed2938182c2fcd43ecbf4ac84ff31951fa
Bartosz Dominiak <smugers@gmail.com>
2020-12-06 17:50:46 +0100

    updated vue-snap url

--- examples/vue@2/vue-cli/src/main.js
+++ examples/vue@2/vue-cli/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueSnap from '../../../../dist/vue-snap.esm'
+import VueSnap from 'vue-snap'
 import 'vue-snap/dist/vue-snap.css'
 
 Vue.use(VueSnap)
